1b39d99b131fca0119fb8915e2dfda84b286b37a
bonmas14 <bonmas14@gmail.com>
2025-01-05 21:23:36 +0300

    changed configurations to real ones. so we can compile right. deleted platform

--- premake5.lua
+++ premake5.lua
@@ -1,67 +1,91 @@
 workspace "solum"
     configurations { "Debug", "Release" }
-    platforms { "native", "llvm" }
+
+    startproject "native-solum-compiler"
 
     warnings "Extra"
     architecture "x86_64"
 
-    defines "_CRT_SECURE_NO_WARNINGS"
-
     filter "configurations:Debug"
-        defines "DEBUG"  
+        defines "DEBUG"
         symbols "On"
         optimize "Debug"
+    filter {}
 
     filter "configurations:Release"
         defines "NDEBUG"
         optimize "Full"
-
-    filter "platforms:llvm" 
-        defines "BACKEND_LLVM" 
-
-    filter "platforms:native" 
-        defines "BACKEND_NATIVE"
-
     filter {}
 
-project "solum-compiler"
+project "native-solum-compiler"
     kind "ConsoleApp"
     language "C"
     cdialect "c11"
 
+    defines "BACKEND_NATIVE"
     files { "./src/**.c" }
-
-    filter "system:windows" 
-        flags { "MultiProcessorCompile" }
-    filter {}
 
     filter "configurations:Debug"
         targetdir "bin"
         targetsuffix "-d"
         staticruntime "on"
         runtime "Debug"
+    filter {}
 
     filter "configurations:Release"
         targetdir "bin"
         targetsuffix "-r"
         staticruntime "on"
         runtime "Release"
+    filter {}
 
-    filter "platforms:llvm" 
-        targetprefix "llvm-"
-filter "system:windows"
-            includedirs { "C:/clang+llvm-18.1.8-x86_64-pc-windows-msvc/include" }
-            libdirs { "C:/clang+llvm-18.1.8-x86_64-pc-windows-msvc/lib" }
-            links { "LLVM-C" }
-            
-        filter "system:linux"  -- i need to find a better way...
-            includedirs { "/usr/lib/llvm-14/include/" } 
-            libdirs { "/usr/lib/llvm-14/lib/" }
-            links { "LLVMCore" }
+    filter "system:windows"
+        flags { "MultiProcessorCompile" }
+        defines "_CRT_SECURE_NO_WARNINGS"
+        -- includedirs { "C:/clang+llvm-18.1.8-x86_64-pc-windows-msvc/include" }
+        -- libdirs { "C:/clang+llvm-18.1.8-x86_64-pc-windows-msvc/lib" }
+        -- links { "LLVM-C" }
+    filter {}
 
-    filter "platforms:native" 
-        targetprefix "native-"
-
+    filter "system:linux"
+        -- includedirs { "/usr/lib/llvm-14/include/" } 
+        -- libdirs { "/usr/lib/llvm-14/lib/" }
+        -- links { "LLVMCore" }
     filter {}
 
 
+project "llvm-solum-compiler"
+    kind "ConsoleApp"
+    language "C"
+    cdialect "c11"
+
+    defines "BACKEND_LLVM"
+    files { "./src/**.c" }
+
+    filter "configurations:Debug"
+        targetdir "bin"
+        targetsuffix "-d"
+        staticruntime "on"
+        runtime "Debug"
+    filter {}
+
+    filter "configurations:Release"
+        targetdir "bin"
+        targetsuffix "-r"
+        staticruntime "on"
+        runtime "Release"
+    filter {}
+
+    filter "system:windows"
+        flags { "MultiProcessorCompile" }
+        defines "_CRT_SECURE_NO_WARNINGS"
+        includedirs { "C:/clang+llvm-18.1.8-x86_64-pc-windows-msvc/include" }
+        libdirs { "C:/clang+llvm-18.1.8-x86_64-pc-windows-msvc/lib" }
+        links { "LLVM-C" }
+    filter {}
+
+    filter "system:linux"
+        includedirs { "/usr/lib/llvm-14/include/" } 
+        libdirs { "/usr/lib/llvm-14/lib/" }
+        links { "LLVMCore" }
+    filter {}
