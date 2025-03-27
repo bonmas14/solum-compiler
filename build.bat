5c3c0cb2443b6723db0b42159a349a47f899be69
bonmas14 <bonmas14@gmail.com>
2025-03-27 16:24:40 +0300

    build script release mode

--- build.bat
+++ build.bat
@@ -1,2 +1,11 @@
+:: @echo off
+:: powershell.exe -noe -c "&{Import-Module """C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell 8b14fce0; cd """C:\Users\bonmas14\projects\solum-compiler"""; msbuild """.\solum.sln""" '/p:Configuration=Debug' '/p:Platform=x64'; exit; }"
+
 @echo off
-powershell.exe -noe -c "&{Import-Module """C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell 8b14fce0; cd """C:\Users\bonmas14\projects\solum-compiler"""; msbuild """.\solum.sln""" '/p:Configuration=Debug' '/p:Platform=x64'; exit; }"
+
+set "config=Debug"
+if "%1"=="Release" (
+    set "config=Release"
+)
+
+powershell.exe -noe -c "&{ Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'; Enter-VsDevShell 8b14fce0; cd 'C:\Users\bonmas14\projects\solum-compiler'; msbuild '.\solum.sln' '/p:Configuration=%config%' '/p:Platform=x64'; exit; }"
